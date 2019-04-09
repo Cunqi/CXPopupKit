5b5dfbeb1b56e9b8fc7c74e5168a05498231d0de
cunqi <cunqi.xiao@gmail.com>
2019-04-09 15:35:30 -0700

    Update CXSpacing

--- Layout/CXSpacing.swift
+++ Layout/CXSpacing.swift
@@ -8,12 +8,13 @@
 import Foundation
 
 enum CXSpacing {
-    static let spacing1: CGFloat = 2
-    static let spacing2: CGFloat = 4
-    static let spacing3: CGFloat = 8
+    static let spacing1: CGFloat = 4
+    static let spacing2: CGFloat = 8
+    static let spacing3: CGFloat = 12
     static let spacing4: CGFloat = 16
-    static let spacing5: CGFloat = 32
-    static let spacing6: CGFloat = 64
-    static let spacing7: CGFloat = 80
-    static let spacing8: CGFloat = 96
+    static let spacing5: CGFloat = 24
+    static let spacing6: CGFloat = 32
+    static let spacing7: CGFloat = 48
+    static let spacing8: CGFloat = 64
+    static let spacing9: CGFloat = 96
 }
